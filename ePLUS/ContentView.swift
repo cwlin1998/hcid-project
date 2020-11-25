e9a0d5ce7d47de62d86265a235825db5de0eec4b
Lin Cheng Wei <snoopy90338@gmail.com>
2020-11-25 17:47:13 +0800

    Initial Commit

---
+++ ePLUS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ePLUS
+//
+//  Created by 林承緯 on 2020/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ePLUS/ePLUSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ePLUSApp.swift
+//  ePLUS
+//
+//  Created by 林承緯 on 2020/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ePLUSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
